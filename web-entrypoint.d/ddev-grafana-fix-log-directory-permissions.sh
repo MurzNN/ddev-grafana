c18459b39266b2479a599584434db8b8fccea557
Alexey Murz Korepov <MurzNN@gmail.com>
2023-05-12 19:51:41 +0400

    Cleanup script

--- web-entrypoint.d/ddev-grafana-fix-log-directory-permissions.sh
+++ web-entrypoint.d/ddev-grafana-fix-log-directory-permissions.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-echo "fix-log-directory-permissions.sh start";
-
 # A workaround to make mounted `/var/log` volume writable for supervisord.
 sudo chmod 777 /var/log
 [ -d /var/log/nginx ] || sudo mkdir /var/log/nginx
 sudo chmod 777 /var/log/nginx
 [ -d /var/log/apache2 ] || sudo mkdir /var/log/apache2
 sudo chmod 777 /var/log/apache2
-
-ls -la /var
-ls -la /var/log
-
-echo "fix-log-directory-permissions.sh end";

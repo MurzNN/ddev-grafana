57b3be9ec7812488550e2ab8cfc32265506b7b0b
Alexey Murz Korepov <MurzNN@gmail.com>
2023-05-12 12:55:14 +0400

    More logs for startup

--- grafana/syslog-ng/syslog-startup.sh
+++ grafana/syslog-ng/syslog-startup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+echo "syslog-startup.sh started";
+
 # A workaround to make mounted `/var/log` volume writable for supervisord.
 sudo chmod 777 /var/log
 [ -d /var/log/nginx ] || sudo mkdir /var/log/nginx
 sudo chmod 777 /var/log/nginx
 [ -d /var/log/apache2 ] || sudo mkdir /var/log/apache2
 sudo chmod 777 /var/log/apache2
+
+ls -la /var
+ls -la /var/log
+
+echo "syslog-startup.sh sleep infinity";
 
 sleep infinity
